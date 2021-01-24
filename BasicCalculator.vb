315d7af452101c9a6a0192bdd0c3a6e46131d34e
Dylan Nelson <dylannelson58@gmail.com>
2021-01-24 10:15:01 -0700

    Add project files.

---
+++ BasicCalculator.vb
@@ -0,0 +1,47 @@
+﻿Option Explicit On
+Option Strict On
+
+'Dylan Nelson
+'RCET0265
+'Spring 2021
+'Say My Name
+'https://github.com/dylannelson58/basiccalculator
+
+Module BasicCalculator
+
+    Sub Main()
+
+        Dim firstNumber As Decimal
+        Dim secondNumber As Decimal
+        Dim thirdNumber As Decimal
+
+        Console.WriteLine("Enter a number!")
+        firstNumber = CDec(Console.ReadLine())
+        Console.WriteLine("Enter another number!")
+        secondNumber = CDec(Console.ReadLine())
+
+        Console.WriteLine("You entered " & firstNumber & " and " & secondNumber & ".")
+        Console.WriteLine("Would you like to add or multiply these numbers?")
+        Console.WriteLine("1 for addition, 2 for multiplication.")
+        thirdNumber = CDec(Console.ReadLine)
+
+        If thirdNumber = 1 Then
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}")
+        End If
+
+        If thirdNumber = 2 Then
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}")
+        End If
+
+        If thirdNumber > 2 Then
+            Console.WriteLine("Please enter 1 or 2 next time.")
+        End If
+
+        Console.WriteLine("Operation complete, have a good day.")
+
+        Console.Read()
+
+
+    End Sub
+
+End Module
